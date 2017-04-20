78460e71ff8d04c3203b04e0ca1166770003be10
Drocco <drocco1@drocco1ml.llnl.gov>
2017-04-20 08:12:42 -0700

    orientation change

--- assets/js/force.js
+++ assets/js/force.js
@@ -69,3 +69,11 @@ var simulation = d3.forceSimulation(nodeData).alphaDecay(0.01).force("collisionF
  }
 
  simulation.on("tick",ticker);
+
+ w.addEventListener('orientationchange', function () {
+   var originalBodyStyle = getComputedStyle(document.body).getPropertyValue('display');
+   document.body.style.display='none';
+   setTimeout(function () {
+     document.body.style.display = originalBodyStyle;
+   }, 10);
+ });
